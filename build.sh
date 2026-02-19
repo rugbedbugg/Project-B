f6613f3ce55862bc7fed2384432988a86bdffab5
Arkixx <yes.par781@gmail.com>
2026-02-19 11:31:13 +0530

    [Build]: Harden build script & Improve failure handling

--- build.sh
+++ build.sh
@@ -8,20 +8,24 @@ echo
 if [ -z "${1:-}" ]; then
 	echo "Usage: ./build.sh <source>"
 	exit 1
-else
-	if [ ! -f "./src/$1.s" ]; then
-		echo "Source file \`$1.s\` not available in \`src/\` directory."
-		echo "Please make sure the file exists and is in the intended location."
-		exit 1
-	fi
-
-	mkdir -p objects/
-	as -o objects/$1.o src/$1.s	&& echo "[+] Assembled \"$1.s\""
-	ld -o $1 objects/$1.o 		&& echo "[+] Linked \"$1.o\""
-	
-	echo "[+] Recompiled webserver \"$1\""
-	echo
-	echo "Run \`./server\` and \`echo \$?\` to verify success."
-
 fi
+
+name="${1%.s}"
+if [ ! -f "./src/$name.s" ]; then
+	echo "Source file \`$name.s\` not available in \`src/\` directory."
+	echo "Please make sure the file exists and is in the intended location."
+	exit 1
+fi
+
+mkdir -p objects/
+
+as -o "objects/$name.o" "src/$name.s"
+echo "[+] Assembled \"$name.s\""
+
+ld -o "$name" "objects/$name.o"
+echo "[+] Linked \"$name.o\""
+
+echo "[+] Built executable \"$name\"" 	\ &&
+echo 					\ &&
+echo "Run \`./$name\` to execute and \`echo \$?\` to verify success."
 
